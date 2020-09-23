157ec79c6d800f553f8a1ca2466144a69ba975e1
karrot <powerkarrot@gmail.com>
2020-09-23 17:02:56 +0200

    cleanup

--- app/Synthesizer.hs
+++ app/Synthesizer.hs
@@ -16,79 +16,20 @@ beat n = beatDuration * n
 f :: Semitone -> Hz
 f n = pitchStandard * (2 ** (1.0 / 12.0)) ** n
 
-sine :: Note -> [Pulse]
-sine n = amplify (_vol n) $ envelope $ sample n
-
-huch n =  concat [map (* (1 - i)) $(sample n) |  i <- [1 .. 10]]
-
-
---distort :: [Pulse] -> [Pulse]
-distort sinew p = map (\x -> signum x * abs x ** p) $ sinew
-
-sawTooth :: Note -> [Pulse]
-sawTooth n = envelope $ map ( subtract $ _vol n ) ( map (/ _vol n) ( map (* pi) a))
-          where a = sample n
-
-
-sawTooth2 n = let a = sample n
-              in envelope $ [ ( _vol n `subtract`) (_vol n / (pi * x)) | x <- a]
-
-
-sawTooth3 n = let a = freq (f (_semitone n)) (_dur n * beatDuration)
-              in envelope $ amplify (_vol n) $ [ 1 - 2 * x | x <- a]
-
-
-squareWave4 n = let a = envelope $ sample n
-                   
-              in  envelope $ amplify (_vol n) $ [ (signum x * 2) -1 | x <- a]
-
-squareWave n = let a = envelope $ sample n
-                   
-              in  envelope $ amplify (_vol n) $ [ signum x| x <- a]
-
-test2 n = let a = envelope $ sample n
-              b = [1, 3 .. 103]
-              c = amplify (_vol n) $ [sum $ map (* x) b | x <- a]
-          in  c
-
-tri n = let a = sample n
-            in  envelope $ map (* _vol n) $ [if (2 - (4 * x)) < 1 then 4 * x else if (4 * x) > 3 then (4 * x) - 4 else 2 * 4 * x| x <- a]
-
---testa
-gibbsWaveAcc n = let a = envelope $ sample n
-                     b = [((negate 2) / (pi * t )) | t <- [ 1 .. 100]]
-                     c = [sum $ map (* (2 * pi * (_vol n) * x)) b | x <- a]
-                in  c
-
-
---lmao not a triagle. 
-triangleWave2 n = let a = envelope $ sample n
-
-                      one = negate $ _vol n  + (( 2 * _vol n) / pi)
-                      two = (3 * _vol n) - ((2 * _vol n) / pi)
-                      d = [if (x < 0) then (x * one) else (negate (x * one)) | x <- a]
-              in  d
-
-triangleWave n = let a = envelope $ sample n
-
-                      one = negate $ (_vol n)  + (( 2 * _vol n) / pi)
-                      d = [if (x < 0) then (x * one) else (negate (x * one)) | x <- a]
-              in  d
-
 
 amplify :: Num b => b -> [b] -> [b]
 amplify v = map (v *)
 
 
-sineWave :: Hz -> Samples
-sineWave hz =  hz * 2 * pi
+sine :: Hz -> Samples
+sine hz =  hz * 2 * pi
 
 --frequency
 freq :: Hz -> Seconds -> [Pulse]
 freq hz duration = output
     where
       step :: Hz
-      step = sineWave hz / sampleRate  --A440 pitch standard
+      step = sine hz / sampleRate  --A440 pitch standard
       output :: [Pulse]
       output = map sin $ map (*step) [ 0.0 .. sampleRate * duration]
 
@@ -103,15 +44,38 @@ envelope line =  zipWith3 (\x y z -> x * y * z) release attack line
 
 
 makeLine :: [Note] -> [Pulse]
-makeLine notes  = concat [triangleWave  n | n <- notes]
+makeLine notes  = concat [triangleWave n| n <- notes]
 
 -- whatever this kind of 3 times list comprehension is called
 makeNote :: [Semitone] -> [Beats] -> [Volume] -> [Note]
 makeNote line dur vol = [Note x y z |(x, y, z) <- zip3 line dur vol]
 
-
+sample :: Note -> [Pulse]
 sample n = freq (f (_semitone n)) (_dur n * beatDuration)
 
+
+sineWave :: Note -> [Pulse]
+sineWave n = amplify (_vol n) $ envelope $ sample n
+
+
+distort :: Floating b => [b] -> b -> [b]
+distort sinew p = map (\x -> signum x * abs x ** p) $ sinew
+
+
+failedSaw :: Note -> [Pulse]
+failedSaw n = let a = envelope $ sample n
+              in  envelope $ amplify (_vol n) $ [ ((-2) / pi ) * x| x <- a]
+
+squareWave :: Note -> [Pulse]
+squareWave n = envelope $ amplify (_vol n) $ map signum (sample n)
+
+triangleWave :: Note -> [Pulse]
+triangleWave n = let a = envelope $ sample n
+
+                     one = (_vol n)  + (( 2 * _vol n) / pi )
+
+                     d = [if (x < 0) then negate (x * one) else  (x * one) | x <- a] 
+              in  d
 
 --test  waves
 test = let 
@@ -121,3 +85,5 @@ test = let
         in saveAsWav b "/home/karrot/test.wav"
         --in b
  
+
+huch n =   [sum $ map (+ (i )) $(sample n) |  i <- [1 .. 10000]]
